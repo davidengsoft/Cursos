5f19f53a2ff50c46562afb48627ad778b05180ce
David <davidob.softeng@gmail.com>
2025-06-22 21:32:49 -0300

    Primeiro exemplo

--- Java/exercicios/src/classe/ProdutoTeste.java
+++ Java/exercicios/src/classe/ProdutoTeste.java
@@ -5,9 +5,25 @@ public class ProdutoTeste {
 
     public static void main(String[] args) {
 
-        int a = 3;
         Produto p1 = new Produto();
-        
+        p1.nome = "Notebook";
+        p1.preco = 4532.80;
+        p1.desconto = 0.25;
+
+        var p2 = new Produto();
+        p2.nome = "Mouse";
+        p2.preco = 123.15;
+        p2.desconto = 0.10;
+
+        System.out.println(p1.nome);
+        System.out.println(p2.nome);
+
+        double precoFinal1 = p1.preco - (p1.preco * p1.desconto);
+        System.out.printf("R$%.2f\n", precoFinal1);
+
+        double precoFinal2 = p2.preco - (p2.preco * p2.desconto);
+        System.out.printf("R$%.2f\n", precoFinal2);
+
 
     }
 }
