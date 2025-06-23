d4333411e9e6232bb61f8b38accc1e21f5cb78c4
David <davidob.softeng@gmail.com>
2025-06-23 20:26:39 -0300

    Desafio data

---
+++ Java/exercicios/src/classe/Data.java
@@ -0,0 +1,8 @@
+package exercicios.src.classe;
+
+public class Data {
+
+    int dia;
+    String mes;
+    int ano;
+}
---
+++ Java/exercicios/src/classe/DataTeste.java
@@ -0,0 +1,18 @@
+package exercicios.src.classe;
+
+public class DataTeste {
+
+    public static void main(String[] args) {
+
+        Data data1 = new Data();
+        data1.ano = 2005;
+        data1.dia = 4;
+        data1.mes = "outubro";
+
+        Data data2 = new Data();
+        data2.ano = 2005;
+        data2.dia = 23;
+        data2.mes = "março";
+
+    }
+}
