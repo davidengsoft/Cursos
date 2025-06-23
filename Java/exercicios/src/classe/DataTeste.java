2b44ea03fdf6344631df721f375ddf1af8a89f84
David <davidob.softeng@gmail.com>
2025-06-23 20:34:31 -0300

    Desafio data

--- Java/exercicios/src/classe/DataTeste.java
+++ Java/exercicios/src/classe/DataTeste.java
@@ -9,10 +9,14 @@ public class DataTeste {
         data1.dia = 4;
         data1.mes = 10;
 
+        System.out.printf("%d/%d/%d", data1.dia, data1.mes, data1.ano);
+
         Data data2 = new Data();
-        data2.ano = 2005;
+        data2.ano = 2025;
         data2.dia = 23;
-        data2.mes = 03;
+        data2.mes = 06;
+
+        System.out.printf("\n%d/%d/%d", data2.dia, data2.mes, data2.ano);
 
     }
 }
