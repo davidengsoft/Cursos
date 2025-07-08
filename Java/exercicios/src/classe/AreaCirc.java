2f72e8f0fceee4390f307ddd9dd16305dd60ca94
David <davidob.softeng@gmail.com>
2025-07-07 21:32:10 -0300

    static em Java

--- Java/exercicios/src/classe/AreaCirc.java
+++ Java/exercicios/src/classe/AreaCirc.java
@@ -3,13 +3,17 @@ package exercicios.src.classe;
 public class AreaCirc {
 
     double raio;
-    static double PI = 3.1415;
+    static final double PI = 3.1415;
 
     AreaCirc(double raioInicial) {
         raio = raioInicial;
     }
 
     double calcularArea() {
+        return Math.pow(raio, 2) * PI;
+    }
+
+    static  double calcularArea(double raio) {
         return Math.pow(raio, 2) * PI;
     }
 }
--- Java/exercicios/src/classe/AreaCircTeste.java
+++ Java/exercicios/src/classe/AreaCircTeste.java
@@ -5,7 +5,11 @@ public class AreaCircTeste {
 
     public static void main(String[] args) {
 
-        AreaCirc area1 = new AreaCirc(5.6);
+        AreaCirc area1 = new AreaCirc(10);
+        AreaCirc area2 = new AreaCirc(5);
+
         System.out.println(area1.calcularArea());
+        System.out.println(area2.calcularArea());
+        System.out.println(AreaCirc.calcularArea(50));
     }
 }
