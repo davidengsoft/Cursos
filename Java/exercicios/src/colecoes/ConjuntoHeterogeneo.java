77ae54aeae6884cce601ed3b0fa371a5d6618cf4
David <davidob.softeng@gmail.com>
2025-07-24 13:54:54 -0300

    Set #1

--- Java/exercicios/src/colecoes/ConjuntoHeterogeneo.java
+++ Java/exercicios/src/colecoes/ConjuntoHeterogeneo.java
@@ -4,7 +4,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 @SuppressWarnings("ALL")
-public class ConjuntoBaguncado {
+public class ConjuntoHeterogeneo {
 
     public static void main(String[] args) {
 
