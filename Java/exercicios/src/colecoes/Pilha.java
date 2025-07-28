d317e02cad45aad030bc769a63e60b43e066c333
David <davidob.softeng@gmail.com>
2025-07-28 15:00:57 -0300

    Stack

---
+++ Java/exercicios/src/colecoes/Pilha.java
@@ -0,0 +1,29 @@
+package exercicios.src.colecoes;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class Pilha {
+
+    public static void main(String[] args) {
+
+        Deque<String> livros = new ArrayDeque<>();
+
+        livros.add("O Pequeno Príncipe");
+        livros.push("Don Quixote");
+        livros.push("O Hobbit");
+
+        System.out.println(livros.peek());
+        System.out.println(livros.element());
+
+        System.out.println(livros.poll());
+        System.out.println(livros.pop());
+        System.out.println(livros.remove());
+
+        System.out.println(livros.size());
+
+        livros.clear();
+        System.out.println(livros.contains("O Pequeno Príncipe"));
+        System.out.println(livros.isEmpty());
+    }
+}
