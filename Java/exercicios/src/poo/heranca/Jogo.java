0f44b1b5c393fd3a2d1a49293c4ff3b596482fa7
David <davidob.softeng@gmail.com>
2025-08-06 14:50:55 -0300

    Herança

--- Java/exercicios/src/poo/heranca/Jogo.java
+++ Java/exercicios/src/poo/heranca/Jogo.java
@@ -7,10 +7,7 @@ public class Jogo {
         Monstro monstro = new Monstro();
         monstro.x = 10;
         monstro.y = 10;
-
-        Heroi heroi = new Heroi();
-        heroi.x = 10;
-        heroi.y = 11;
+        Heroi heroi = new Heroi(10, 11);
 
         System.out.println("Monstro tem => " + monstro.vida);
         System.out.println("Heroi tem   => " + heroi.vida);
--- Java/exercicios/src/poo/heranca/Monstro.java
+++ Java/exercicios/src/poo/heranca/Monstro.java
@@ -1,4 +1,8 @@
 package exercicios.src.poo.heranca;
 
 public class Monstro extends Jogador{
+
+    Monstro() {
+        super(0, 0);
+    }
 }
