2c64ff551e92ce6c0cc96b9d66c65c4f61e8b699
David <davidob.softeng@gmail.com>
2025-08-13 10:42:50 -0300

    Predicate

---
+++ Java/exercicios/src/Lambdas/PredicadoComposicao.java
@@ -0,0 +1,15 @@
+package exercicios.src.Lambdas;
+
+import java.util.function.Predicate;
+
+public class PredicadoComposicao {
+
+    public static void main(String[] args) {
+
+        Predicate<Integer> isPar = numero -> numero % 2 == 0;
+        Predicate<Integer> isTresDigitos = numero -> numero >= 100 && numero <= 999;
+
+        System.out.println(isPar.and(isTresDigitos).test(153));
+        System.out.println(isPar.or(isTresDigitos).test(153));
+    }
+}
