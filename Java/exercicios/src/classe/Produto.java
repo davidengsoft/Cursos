a26c3c976b40c637f3d0b842aaecee7f6c976825
David <davidob.softeng@gmail.com>
2026-02-11 13:56:28 -0300

    Criando produtos

--- Java/exercicios/src/classe/Produto.java
+++ Java/exercicios/src/classe/Produto.java
@@ -4,20 +4,7 @@ public class Produto {
 
     String nome;
     double preco;
-    static double desconto = 0.25;
-
-    Produto() {
-
-    }
-
-    Produto(String nomeInicial) {
-        nome = nomeInicial;
-    }
-
-
-    double precoComDesconto() {
-        return preco - (preco * desconto);
-    }
+    double desconto;
 
 
 }
--- Java/exercicios/src/classe/ProdutoTeste.java
+++ Java/exercicios/src/classe/ProdutoTeste.java
@@ -5,22 +5,15 @@ public class ProdutoTeste {
 
     public static void main(String[] args) {
 
-        Produto p1 = new Produto("Notebook");
-        p1.preco = 4532.80;
+        Produto p1 = new Produto();
+        p1.nome = "Notebook";
+        p1.preco = 4356.89;
+        p1.desconto = 0.25;
 
-        var p2 = new Produto();
-        p2.nome = "Mouse";
-        p2.preco = 123.15;
-
-        System.out.println(p1.nome);
-        System.out.println(p2.nome);
-
-        double precoFinal1 = p1.precoComDesconto();
-        System.out.printf("R$%.2f\n", precoFinal1);
-
-        double precoFinal2 = p2.precoComDesconto();
-        System.out.printf("R$%.2f\n", precoFinal2);
-
+        Produto p2 = new Produto();
+        p2.nome = "Caneta preta";
+        p2.preco = 12.56;
+        p2.desconto = 0.29;
 
     }
 }
