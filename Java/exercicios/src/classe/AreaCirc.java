9c2b9fcfb05cb16bcf455e3e91feedaf9c6f75dc
David <davidob.softeng@gmail.com>
2026-02-13 13:23:57 -0300

    Atributos de classe e atributos de instâncias

--- Java/exercicios/src/classe/AreaCirc.java
+++ Java/exercicios/src/classe/AreaCirc.java
@@ -2,18 +2,16 @@ package exercicios.src.classe;
 
 public class AreaCirc {
 
-    double raio;
-    static final double PI = 3.1415;
+    double raio; //Atributo de instância. Cada objeto terá seu próprio valor.
+    final static double PI = 3.1415; //Atributo de classe.
+                                     // O valor definido é o mesmo para todos objetos
+                                     // (palavra chave: static)
 
-    AreaCirc(double raioInicial) {
-        raio = raioInicial;
+    AreaCirc(double raio) {
+        this.raio = raio;
     }
 
-    double calcularArea() {
-        return Math.pow(raio, 2) * PI;
-    }
-
-    static  double calcularArea(double raio) {
+    double area() {
         return Math.pow(raio, 2) * PI;
     }
 }
--- Java/exercicios/src/classe/AreaCircTeste.java
+++ Java/exercicios/src/classe/AreaCircTeste.java
@@ -5,11 +5,13 @@ public class AreaCircTeste {
 
     public static void main(String[] args) {
 
-        AreaCirc area1 = new AreaCirc(10);
-        AreaCirc area2 = new AreaCirc(5);
+        AreaCirc area01 = new AreaCirc(10);
+        System.out.println(area01.area());
 
-        System.out.println(area1.calcularArea());
-        System.out.println(area2.calcularArea());
-        System.out.println(AreaCirc.calcularArea(50));
+
+        AreaCirc area02 = new AreaCirc(5.74);
+        System.out.println(area02.area());
+
+        System.out.println("Valor de PI: " + AreaCirc.PI);
     }
 }
