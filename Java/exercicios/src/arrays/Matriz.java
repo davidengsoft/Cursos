f0bc08bcbacebfc8c38bdda557a49eb13207f50f
David <davidob.softeng@gmail.com>
2026-02-17 17:18:49 -0300

    Utilizandos matrizes

--- Java/exercicios/src/arrays/Matriz.java
+++ Java/exercicios/src/arrays/Matriz.java
@@ -7,34 +7,38 @@ public class Matriz {
 
     public static void main(String[] args) {
 
-        double total = 0, media;
+        int quantidDeAlunos;
+        int quantidDeNotas;
+        double total = 0;
+        double media;
+        double[][] notasDaTurma;
+
         Scanner entrada = new Scanner(System.in);
 
         System.out.println("Quantos alunos? ");
-        int quantAlunos = entrada.nextInt();
+        quantidDeAlunos = entrada.nextInt();
 
         System.out.println("Quantas notas por aluno? ");
-        int quantNotas = entrada.nextInt();
+        quantidDeNotas = entrada.nextInt();
 
-        double[][] notasDaTurma = new double[quantAlunos][quantNotas];
+        notasDaTurma = new double[quantidDeAlunos][quantidDeNotas];
 
-        for (int a = 0; a < notasDaTurma.length; a++) {
-            for (int n = 0; n < notasDaTurma[a].length; n++) {
-                System.out.printf("Informe a nota %d do aluno %d: ", n + 1, a + 1);
-                notasDaTurma[a][n] = entrada.nextDouble();
-                total += notasDaTurma[a][n];
+        for (int i = 0; i < notasDaTurma.length; i++) {
+            for (int j = 0; j < notasDaTurma[i].length; j++) {
+                System.out.printf("Informe a nota %d do aluno %d: ", j + 1, i + 1);
+                notasDaTurma[i][j] = entrada.nextDouble();
+                total += notasDaTurma[i][j];
             }
         }
 
-        media = total / (quantAlunos * quantNotas);
-        System.out.printf("A média da turma é %.2f\n", media);
+        media = total / (quantidDeAlunos * quantidDeNotas);
+        System.out.println("A média da turma é: " + media);
 
-        for(double[] notasDoAluno: notasDaTurma) {
+        for (double[] notasDoAluno : notasDaTurma) {
             System.out.println(Arrays.toString(notasDoAluno));
         }
 
         entrada.close();
-
 
 
     }
