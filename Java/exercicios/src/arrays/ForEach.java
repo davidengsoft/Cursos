8e3f82ae265f08e54069f85b1a8da572e9ecaaa6
David <davidob.softeng@gmail.com>
2026-02-17 16:11:17 -0300

    Usando Foreach

--- Java/exercicios/src/arrays/ForEach.java
+++ Java/exercicios/src/arrays/ForEach.java
@@ -4,10 +4,11 @@ public class ForEach {
 
     public static void main(String[] args) {
 
-        double[] notas = {9.9, 8.7, 7.2, 9.4};
+        double[] notas = {9.9, 8.7, 9.4, 7.2 };
 
-        for(double nota: notas) {
+        for (double nota : notas) {
             System.out.println(nota);
         }
+
     }
 }
