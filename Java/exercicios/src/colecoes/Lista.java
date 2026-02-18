b325ec7076868e70b713c3ef70b48a703c16b76f
David <davidob.softeng@gmail.com>
2026-02-18 15:06:19 -0300

    Collection List

--- Java/exercicios/src/colecoes/Lista.java
+++ Java/exercicios/src/colecoes/Lista.java
@@ -1,39 +1,26 @@
 package exercicios.src.colecoes;
 
 import java.util.ArrayList;
-import java.util.List;
+
 
 public class Lista {
 
     public static void main(String[] args) {
 
-        List<Usuario> lista = new ArrayList<>();
+        ArrayList<Usuario> lista = new ArrayList<>();
 
         Usuario u1 = new Usuario("Ana");
         lista.add(u1);
 
         lista.add(new Usuario("David"));
-        lista.add(new Usuario("Carlos"));
-        lista.add(new Usuario("José"));
-        lista.add(new Usuario("Rebeca"));
+        lista.add(new Usuario("Lia"));
+        lista.add(new Usuario("Gloria"));
+        lista.add(new Usuario("Manu"));
 
-        System.out.println(lista.get(2));
+        System.out.println(lista.get(3));
 
-        for( Usuario u: lista) {
-            System.out.println(u.nome);
+        for (Usuario usuario : lista) {
+            System.out.println(usuario.nome);
         }
-
-        System.out.println("-------");
-
-        lista.remove(4);
-        lista.remove(new Usuario("Ana"));
-        for(Usuario u: lista) {
-            System.out.println(u.nome);
-        }
-
-        System.out.println("-------");
-        System.out.println("Tem? " + lista.contains(new Usuario("David")));
-        System.out.println("Tem? " + lista.contains(new Usuario("Rebeca")));
-
     }
 }
--- Java/exercicios/src/colecoes/Usuario.java
+++ Java/exercicios/src/colecoes/Usuario.java
@@ -4,24 +4,25 @@ import java.util.Objects;
 
 public class Usuario {
 
-
     String nome;
 
     Usuario(String nome) {
         this.nome = nome;
     }
 
+    @Override
     public String toString() {
         return "Meu nome é " + this.nome + ".";
+    }
+
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Usuario usuario = (Usuario) o;
+        return Objects.equals(nome, usuario.nome);
     }
 
 
     public int hashCode() {
         return Objects.hashCode(nome);
-    }
-
-    public boolean equals(Object obj) {
-        Usuario usuario = (Usuario) obj;
-        return this.nome.equals(usuario.nome);
     }
 }
