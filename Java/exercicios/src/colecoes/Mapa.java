1dc4bf1411de3be0f233ccb5a11a7f32065fe416
David <davidob.softeng@gmail.com>
2026-02-19 10:31:24 -0300

    Collection Map

--- Java/exercicios/src/colecoes/Mapa.java
+++ Java/exercicios/src/colecoes/Mapa.java
@@ -1,6 +1,7 @@
 package exercicios.src.colecoes;
 
 import java.util.HashMap;
+import java.util.Map.Entry;
 import java.util.Map;
 
 public class Mapa {
@@ -10,33 +11,40 @@ public class Mapa {
         Map<Integer, String> usuarios = new HashMap<>();
 
         usuarios.put(1, "Roberto");
-        usuarios.put(2, "Rafaela");
-        usuarios.put(3, "Rafael");
+        usuarios.put(2, "Ricardo");
+        usuarios.put(3, "Rafaela");
         usuarios.put(4, "Rebeca");
-
 
         System.out.println(usuarios.size());
         System.out.println(usuarios.isEmpty());
-
         System.out.println(usuarios.keySet());
         System.out.println(usuarios.values());
         System.out.println(usuarios.entrySet());
 
-        System.out.println(usuarios.containsKey(2));
+        System.out.println(usuarios.containsKey(20));
         System.out.println(usuarios.containsValue("Rebeca"));
 
-        System.out.println(usuarios.get(2));
+        System.out.println(usuarios.get(4));
 
-        for (int chave: usuarios.keySet()) {
+        System.out.println("===================");
+
+        for (int chave : usuarios.keySet()) {
             System.out.println(chave);
         }
 
-        for (String valores: usuarios.values()) {
-            System.out.println(valores);
+        System.out.println("===================");
+
+
+        for (String valor : usuarios.values()) {
+            System.out.println(valor);
         }
 
-        for (Map.Entry<Integer, String> registro: usuarios.entrySet()){
-            System.out.println(registro);
+        System.out.println("===================");
+
+
+        for (Entry<Integer, String> registro : usuarios.entrySet()) {
+            System.out.print(registro.getKey() + " -> ");
+            System.out.println(registro.getValue());
         }
     }
 }
