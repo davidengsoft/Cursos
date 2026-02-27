5a8deee8f9c8faa417aaafea62094368cb748ef6
David <davidob.softeng@gmail.com>
2026-02-27 19:15:12 -0300

    Praticando herança com uma simulação de jogo

--- Java/exercicios/src/poo/heranca/Heroi.java
+++ Java/exercicios/src/poo/heranca/Heroi.java
@@ -3,10 +3,8 @@ package exercicios.src.poo.heranca;
 public class Heroi extends Jogador{
 
     public Heroi(int x, int y){
-        this(x, y);
+        super(x, y);
     }
-
-
     @Override
     public boolean atacar(Jogador oponente) {
         int deltaX = Math.abs(x - oponente.x);
